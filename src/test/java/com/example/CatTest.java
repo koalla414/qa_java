0d0c7c56147e2be7432b26a07af4f280e78aa452
koalla414 <koalla414@yandex.ru>
2022-04-17 23:58:26 +0300

    Sprint2

---
+++ src/test/java/com/example/CatTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class CatTest {
+
+    @Test
+    public void getSoundIsMeow() {
+        String expected = "Мяу";
+        Feline feline = new Feline();
+        Cat cat = new Cat(feline);
+        String actual = cat.getSound();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getFoodReturnMenuOfPredator() throws Exception {
+
+        Feline feline = new Feline();
+        Cat cat = new Cat(feline);
+
+        List<String> expectedMenu = feline.eatMeat();
+        List<String> actualMenu = cat.getFood();
+
+        Assert.assertEquals("Меню определено неверно", expectedMenu, actualMenu);
+    }
+
+}
---
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -0,0 +1,39 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class LionParameterizedTest {
+
+    private final String sexLion;
+    private final boolean expected;
+
+    public LionParameterizedTest(String sexLion, boolean expected) {
+        this.sexLion = sexLion;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] hasManeLion() {
+        return new Object[][] {
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+    @Test
+    public void shouldBeKittensValue() throws Exception {
+
+        Feline feline = new Feline();
+        Lion lion = new Lion(sexLion, feline);
+
+        boolean actual = lion.doesHaveMane();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+}
+
+
